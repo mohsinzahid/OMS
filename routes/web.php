b1fe6e63471808f368c9630e94db0b08794f0228
MohsinZahid <mohsinzahid16@gmail.com>
2019-09-20 12:51:15 +0500

    testing for show jobs

--- routes/web.php
+++ routes/web.php
@@ -495,7 +495,7 @@ Route::get('/customer/saleprice/ajaxupdate','CustomerController@show');
 
     Route::get('/sales/reports/customerLedgerReport',function ()
     {
-        if (Auth::user()->type_id === 1 || Auth::user()->type_id === 2)
+        if (Auth::user()->type_id === 1 || Auth::user()->type_id === 2 || Auth::user()->type_id === 3)
             return app()->call('App\Http\Controllers\SalesReportsController@CustomerLedgerReport');
         else
             return view('admin.master');
@@ -506,7 +506,7 @@ Route::get('/sales/reports/customerLedgerReport/ajaxupdate', 'SalesReportsContro
     Route::get('/reports/customerLedgerDetail/ajaxupdate', 'SalesReportsController@CustomerLedgerDetailAjaxUpdate');
 
     Route::get('/reports/customerLedgerDetail', function () {
-        if (Auth::user()->type_id === 1 || Auth::user()->type_id === 2)
+        if (Auth::user()->type_id === 1 || Auth::user()->type_id === 2 || Auth::user()->type_id === 3)
             return app()->call('App\Http\Controllers\SalesReportsController@statement');
         else
             return view('admin.master');
