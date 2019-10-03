71eca7342e9f75962f10c0768e9f49806e064e4c
MohsinZahid <mohsinzahid16@gmail.com>
2019-10-03 15:14:45 +0500

    customer name and type added in general ledger report

--- app/Http/Controllers/GeneralLedgerReportsController.php
+++ app/Http/Controllers/GeneralLedgerReportsController.php
@@ -209,9 +209,9 @@ class GeneralLedgerReportsController extends Controller
                 (DB::raw("CASE WHEN ci.tax_amount iS NULL THEN sp.amount ELSE sp.amount + ci.tax_amount END as debit_amount")),
                 DB::raw("0 as credit_amount"), DB::raw("'CC' as formtype"),"sp.added_at as added_at", "ci.cheque_no as cheque_no",
                 "ci.cheque_date as cheque_date",DB::raw("'' as created_by"),DB::raw("'' as remarks"),
-                DB::raw("CASE WHEN sp.invoiceno iS NULL THEN CASE WHEN c.type = 0 THEN w.name ELSE c.name END ELSE '' as customer_name"),
-                DB::raw("CASE WHEN sp.invoiceno iS NULL THEN CASE WHEN c.type = 0 THEN 'Walk In Customer' ELSE 
-                    'Credit customer' END ELSE '' as customer_type"))
+                DB::raw("CASE WHEN sp.invoiceno iS NULL AND c.type = 0 THEN w.name WHEN sp.invoiceno iS NULL AND c.type != 0 THEN c.name ELSE '' END as customer_name"),
+                /*DB::raw("CASE WHEN sp.invoiceno iS NULL THEN CASE WHEN c.type = 0 THEN 'Walk In Customer' ELSE
+                    'Credit customer' END ELSE '' as customer_type")*/ DB::raw("'' as customer_type"))
             ->where('sp.date', '>=', $request['start'])
             ->where('sp.date', '<=', $request['end']);
 
