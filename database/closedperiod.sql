197bfb01861563084ba314413848f749c77cbcbb
MohsinZahid <mohsinzahid16@gmail.com>
2019-10-08 00:21:17 +0500

    closed period file added

---
+++ database/closedperiod.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 07, 2019 at 09:36 PM
+-- Server version: 5.7.27-0ubuntu0.18.04.1
+-- PHP Version: 7.1.32-1+ubuntu18.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `closedperiod`
+--
+
+CREATE TABLE `closedperiod` (
+  `id` tinyint(4) NOT NULL,
+  `closed_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `closedperiod`
+--
+
+INSERT INTO `closedperiod` (`id`, `closed_date`) VALUES
+(1, '2019-10-05');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
