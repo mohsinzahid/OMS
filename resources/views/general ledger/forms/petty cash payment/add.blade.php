64334331f33a67adfd42ea2d377f02fc697ad802
MohsinZahid <mohsinzahid16@gmail.com>
2019-10-08 15:28:45 +0500

    closed period front end form added

--- resources/views/general ledger/forms/petty cash payment/add.blade.php
+++ resources/views/general ledger/forms/petty cash payment/add.blade.php
@@ -35,7 +35,7 @@
                             <div class="row">
                                 <div class="form-group col-sm-6">
                                     <label for="Inputdate">Date</label>
-                                    <input type="date" class="form-control" id="Inputdate" name="date" required>
+                                    <input type="date" class="form-control" id="date" name="date" onchange="validate()" required>
                                 </div>
                                 <div class="form-group col-sm-6">
                                     <label for="Inputamount">Amount</label>
@@ -50,7 +50,7 @@
                                            name="remarks" required>
                                 </div>
                             </div>
-                            <button type="submit" class="btn btn-warning">Submit</button>
+                            <button type="submit" id="submit" class="btn btn-warning" disabled>Submit</button>
 
                         </form>
                     </div>
@@ -60,4 +60,46 @@
             <div class="col-sm-3"></div>
         </div>
     </div>
+
+    <script>
+        function validate() {
+            var date =$("#date").val();
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                statusCode: {
+                    500: function () {
+                        alert("Script exhausted");
+                    }
+                },
+                type: 'GET',
+                url: '/receipts/ajax-validate-period',
+                data: {date: date},
+
+                success: function (response) {
+                    // console.log(response);
+                    if (response === 1) {
+                        $("#submit").removeAttr("disabled").removeClass("btn-default").addClass("btn-success");
+                    }
+                    else
+                    {
+                        toastr.warning('Selected Date is in closed period.');
+                        $("#submit").attr("disabled", true).removeClass("btn-success").addClass("btn-default");
+                    }
+                },
+                error: function (XMLHttpRequest, jqXHR, textStatus, errorThrown) {
+                    if (XMLHttpRequest.readyState == 0) {
+                        // Network error (i.e. connection refused, access denied due to CORS, etc.)
+                        toastr.error('Network Connection Refused');
+                    }
+                    console.log(XMLHttpRequest);
+                    console.log(JSON.stringify(jqXHR));
+                    console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
+                }
+            });
+        }
+    </script>
 @endsection
