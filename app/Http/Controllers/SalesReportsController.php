c5edeb36f01e996e77c7aad75c46828f8e9b800f
MohsinZahid <mohsinzahid16@gmail.com>
2020-09-01 19:53:56 +0500

    Customer Receivable Report update

--- app/Http/Controllers/SalesReportsController.php
+++ app/Http/Controllers/SalesReportsController.php
@@ -181,8 +181,8 @@ class SalesReportsController extends Controller
                 ->first();
 //                ->first(DB::raw('CASE WHEN ci.tax_amount iS NULL THEN sp.amount ELSE sp.amount + ci.tax_amount END'));
 //            $data['lastPaymentAmount'] = $last->amount +  $last->tax_amount;
-            $data['lastPaymentAmount'] = $last['amount'];
-            $data['lastPaymentDate'] = $last['tax_amount'];
+            $data['lastPaymentAmount'] = $last->amount;
+            $data['lastPaymentDate'] = '';
             $data['last'] = $last;
             $result[] = $data;
         }
